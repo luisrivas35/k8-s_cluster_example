6df85bc78def64058e2a75fc02635b85efabbe3e
Luis Rivas <xluisrivas@gmail.com>
2023-09-27 18:34:48 +0000

    update main.tf - add vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+resource "aws_vpc" "cluster_vpc" {
+  cidr_block = "10.0.0.0/16"  # Replace with your desired CIDR block
+}
+
 resource "aws_subnet" "cluster_subnet" {
   vpc_id                  = aws_vpc.cluster_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.1.0/24"  # Replace with your desired CIDR block for the subnet
   availability_zone       = "us-east-1a"  # Replace with your desired AZ
 }
 
@@ -15,4 +19,5 @@ resource "aws_instance" "instance2" {
   instance_type = var.instance_type
   subnet_id     = aws_subnet.cluster_subnet.id
 }
+
 
