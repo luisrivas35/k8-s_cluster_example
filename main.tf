ce884aa5913b7e8d7065beb9a0d2756aa85a2227
Luis Rivas <xluisrivas@gmail.com>
2023-09-28 15:49:08 +0000

    updating

--- main.tf
+++ main.tf
@@ -1,83 +1,99 @@
+# Create the VPC
 resource "aws_vpc" "cluster_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Create a public subnet associated with the Internet Gateway
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.cluster_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  vpc_id             = aws_vpc.cluster_vpc.id
+  cidr_block         = "10.0.1.0/24"
+  availability_zone  = "us-east-1a"
+  map_public_ip_on_launch = true  # This enables instances in this subnet to get public IPs
 }
 
+# Create a private subnet
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.cluster_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  vpc_id             = aws_vpc.cluster_vpc.id
+  cidr_block         = "10.0.2.0/24"
+  availability_zone  = "us-east-1b"
 }
 
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.cluster_vpc.id
+}
+
+# Create the bastion host
 resource "aws_instance" "bastion_host" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_subnet.id
-  key_name      = "clase_key"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  key_name               = "clase_key"
+  associate_public_ip_address = true  # Ensure a public IP is assigned
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get install -y curl wget unzip 
+    sudo apt-get install -y git 
+    sudo apt-get install python3 python3-pip -y
+    sudo apt-get install -y openssh-server
+    sudo systemctl start sshd
+    sudo systemctl enable sshd
+  EOF
 
   tags = {
     Name = "bastion"
   }
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
-    sudo apt-get install -y curl wget unzip 
-    sudo apt-get install -y git 
-    sudo apt-get install python3 python3-pip -y
-    sudo apt-get install -y openssh-server
-    sudo systemctl start sshd
-    sudo systemctl enable sshd
-  EOF
+  # Create a security group for the bastion host allowing SSH access
+  security_groups = [
+    aws_security_group.bastion_sg.id
+  ]
 }
 
-resource "aws_instance" "instance1" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.private_subnet.id
-  key_name      = "clase_key"
-
-  tags = {
-    Name = "master"
-  }
+# Create two instances in the private subnet
+resource "aws_instance" "private_instance1" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private_subnet.id
+  key_name               = "clase_key"
+  associate_public_ip_address = false  # Private instances do not need a public IP
 
   user_data = <<-EOF
     #!/bin/bash
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
-    sudo apt-get install -y curl wget unzip 
-    sudo apt-get install -y git 
-    sudo apt-get install python3 python3-pip -y
-    sudo apt-get install -y openssh-server
-    sudo systemctl start sshd
-    sudo systemctl enable sshd
+    # Configure your private instance here
+    echo "Private instance 1 configured." >> /tmp/private_instance.log
   EOF
+
+  tags = {
+    Name = "private_instance1"
+  }
+
+  # Allow SSH access from the bastion host
+  vpc_security_group_ids = [aws_security_group.private_instance_sg.id]
 }
 
-resource "aws_instance" "instance2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.private_subnet.id
-  key_name      = "clase_key"
-
-  tags = {
-    Name = "worker"
-  }
+resource "aws_instance" "private_instance2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private_subnet.id
+  key_name               = "clase_key"
+  associate_public_ip_address = false  # Private instances do not need a public IP
 
   user_data = <<-EOF
     #!/bin/bash
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
-    sudo apt-get install -y curl wget unzip 
-    sudo apt-get install -y git 
-    sudo apt-get install python3 python3-pip -y
+    # Configure your private instance here
+    echo "Private instance 2 configured." >> /tmp/private_instance.log
   EOF
+
+  tags = {
+    Name = "private_instance2"
+  }
+
+  # Allow SSH access from the bastion host
+  vpc_security_group_ids = [aws_security_group.private_instance_sg.id]
 }
 
 # Security group for the bastion host allowing SSH access
@@ -102,18 +118,4 @@ resource "aws_security_group" "private_instance_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
   }
-}
-
-# Associate the security group with the bastion host
-resource "aws_instance" "bastion_host" {
-  security_groups = [aws_security_group.bastion_sg.id]
-}
-
-# Associate the security group with the private instances
-resource "aws_instance" "instance1" {
-  security_groups = [aws_security_group.private_instance_sg.id]
-}
-
-resource "aws_instance" "instance2" {
-  security_groups = [aws_security_group.private_instance_sg.id]
 }
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,32 @@
-output "instance1_public_ip" {
-  value = aws_instance.instance1.public_ip
+# Outputs
+output "vpc_id" {
+  value = aws_vpc.cluster_vpc.id
 }
 
-output "instance2_public_ip" {
-  value = aws_instance.instance2.public_ip
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "bastion_host_id" {
+  value = aws_instance.bastion_host.id
 }
 
 output "bastion_host_public_ip" {
   value = aws_instance.bastion_host.public_ip
+}
+
+output "bastion_security_group_id" {
+  value = aws_security_group.bastion_sg.id
+}
+
+output "private_instance_security_group_id" {
+  value = aws_security_group.private_instance_sg.id
 }
